df25eae5d97e1d1912f48c63057e4e41317968bc
Paco <paco.brevers@ipronics.com>
2022-02-25 11:59:14 +0100

    Types, shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -4,18 +4,27 @@ fn main() {
     x = 6;
     println!("The value of x if: {}", x);
     shadowing();
+    shadowing_vs_let();
 }
 
 fn shadowing() {
     let x = 5;
-    
-    let x = x +1;
+
+    let x = x + 1;
 
     {
-        let x = x *2;
+        let x = x * 2;
         println!("The value of x is: {}", x);
     }
 
     println!("The value of x is: {}", x);
+}
 
+fn shadowing_vs_let() {
+    let spaces = "    ";
+    let spaces = spaces.len();
+
+    // But not
+    // let mut spaces = "     ";
+    // let spaces = spaces.len();
 }
