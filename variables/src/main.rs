40f9ae79fe9dbc308b294a3145330222567bd5af
Paco <paco.brevers@ipronics.com>
2022-02-25 11:53:04 +0100

    Shadowing example

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,21 @@
-fn main(){
-    let x=5;
-    println("The value of x is: {}", x);
-    let x=6
-    println("The value of x if: {}, x");
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x if: {}", x);
+    shadowing();
+}
+
+fn shadowing() {
+    let x = 5;
+    
+    let x = x +1;
+
+    {
+        let x = x *2;
+        println!("The value of x is: {}", x);
+    }
+
+    println!("The value of x is: {}", x);
+
 }
